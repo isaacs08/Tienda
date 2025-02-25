a6bb53c83eb1ea13d486caf8ececb19a51ef9749
isaacs08 <isalazarch@hotmail.com>
2025-02-25 17:24:42 -0600

    Commit Semana 6
    Commit Semana 6 - antes de Caso Práctico

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
  */
-
 package com.tienda.controller;
 
 import org.springframework.stereotype.Controller;
@@ -18,9 +17,10 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index(Model model) {
-        //model.addAttribute("attribute", "value");
+        model.addAttribute("attribute", "value");
         return "index";
     }
+
     @RequestMapping("/informacion")
     public String contacto(Model model) {
         //model.addAttribute("attribute", "value");
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,5 +1,3 @@
-
-
 package com.tienda.service.impl;
 
 import com.tienda.dao.CategoriaDao;
@@ -8,24 +6,39 @@ import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaServiceImpl implements CategoriaService {
-    
+
     @Autowired
     private CategoriaDao categoriaDao;
-    
-    public List<Categoria> getCategorias(boolean activos){
-        var lista = categoriaDao.findAll();
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
         if (activos) {
-            lista.removeIf(c -> !c.isActivo());
-        
+            lista.removeIf(e -> !e.isActivo());
         }
-        
-             return lista;
-        
+        return lista;
     }
-    
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.deleteById(categoria.getIdCategoria());
+    }
 }
